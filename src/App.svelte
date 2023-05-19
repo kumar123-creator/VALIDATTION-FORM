5149f21c1ddf2d01995d0b6901d2c817ec5eb7ab
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-05-19 17:46:07 +0530

    code updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,161 +1,105 @@
 
-<h1>Enter Your Details</h1>
+<h1>DETAILS</h1>
 <script>
-  import { createEventDispatcher } from 'svelte';
-  import 'bootstrap/dist/css/bootstrap.min.css';
+  import { onMount } from "svelte";
 
-
-  const dispatch = createEventDispatcher();
-
-  let firstName = '';
-  let surname = '';
-  let email = '';
-  let password = '';
-  let mobile = '';
-
-  let errors = {
-    firstName: '',
-    surname: '',
-    email: '',
-    password: '',
-    mobile: ''
+  let formData = {
+    name: "",
+    email: "",
+    password: "",
   };
 
-  let submissionStatus = '';
-  let submissionData = {};
+  let errors = {};
 
   const validateForm = () => {
-    let valid = true;
-    errors = {
-      firstName: '',
-      surname: '',
-      email: '',
-      password: '',
-      mobile: ''
-    };
+    errors = {};
 
-    // Validate First Name
-    if (firstName.length === 0) {
-      errors.firstName = 'First Name is required';
-      valid = false;
-    } else if (firstName.length > 28) {
-      errors.firstName = 'First Name cannot exceed 28 characters';
-      valid = false;
+    if (!formData.name) {
+      errors.name = "Name is required";
     }
 
-    // Validate Surname
-    if (surname.length === 0) {
-      errors.surname = 'Surname is required';
-      valid = false;
-    } else if (surname.length > 28) {
-      errors.surname = 'Surname cannot exceed 28 characters';
-      valid = false;
+    if (!formData.email) {
+      errors.email = "Email is required";
+    } else if (!isValidEmail(formData.email)) {
+      errors.email = "Invalid email format";
     }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (email.length === 0) {
-      errors.email = 'Email is required';
-      valid = false;
-    } else if (!emailRegex.test(email)) {
-      errors.email = 'Invalid email format';
-      valid = false;
-    }
-
-    const passwordRegex = /^(?=.*[A-Za-z0-9])(?=.*[!@#$%^&*()])[A-Za-z0-9!@#$%^&*()]{6,15}$/;
-    if (password.length === 0) {
-      errors.password = 'Password is required';
-      valid = false;
-    } else if (!passwordRegex.test(password)) {
-      errors.password = 'Your password must have at least eight characters long, one uppercase, one lowercase character, and one number';
-      valid = false;
-    }
-
-    const mobileRegex = /^[0-9+]+$/;
-    if (mobile.length === 0) {
-      errors.mobile = 'Mobile is required';
-      valid = false;
-    } else if (!mobileRegex.test(mobile)) {
-      errors.mobile = 'Mobile Number is incorrect format';
-      valid = false;
-    }
-
-    if (valid) {
-      // Submit the form
-      submissionStatus = 'success';
-      submissionData = {
-        firstName,
-        surname,
-        email,
-        password,
-        mobile
-      };
-      dispatch('submit', submissionData);
+    if (!formData.password) {
+      errors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      errors.password = "Password must be at least 6 characters long";
     }
   };
+
+  const isValidEmail = (email) => {
+    // Regular expression to validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = () => {
+    validateForm();
+
+    if (Object.keys(errors).length === 0) {
+      // Form submission logic goes here
+      console.log("Form submitted successfully");
+    }
+  };
+
+  onMount(() => {
+    // Validate the form on initial load
+    validateForm();
+  });
 </script>
 
-
-
 <style>
-   @import 'bootstrap/dist/css/bootstrap.min.css';
+  .error {
+    color: red;
+  }
 </style>
 
-
-
-
-<div class="container mt-6">
-  <form on:submit|preventDefault={validateForm}>
+<div class="container mt-5">
+  <form on:submit|preventDefault={handleSubmit}>
     <div class="form-group">
-      <label for="firstName">First Name:</label>
-      <input type="text" id="firstName" class="form-control" bind:value={firstName} />
-      {#if errors.firstName}
-        <p class="error">{errors.firstName}</p>
+      <label for="name">Name</label>
+      <input
+        type="text"
+        class="form-control"
+        id="name"
+        bind:value={formData.name}
+      />
+      {#if errors.name}
+        <p class="error">{errors.name}</p>
       {/if}
     </div>
 
     <div class="form-group">
-      <label for="surname">Surname:</label>
-      <input type="text" id="surname" class="form-control" bind:value={surname} />
-      {#if errors.surname}
-        <p class="error">{errors.surname}</p>
-      {/if}
-    </div>
-
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" id="email" class="form-control" bind:value={email} />
+      <label for="email">Email</label>
+      <input
+        type="email"
+        class="form-control"
+        id="email"
+        bind:value={formData.email}
+      />
       {#if errors.email}
         <p class="error">{errors.email}</p>
       {/if}
     </div>
 
     <div class="form-group">
-      <label for="password">Password:</label>
-      <input type="password" id="password" class="form-control" bind:value={password} />
+      <label for="password">Password</label>
+      <input
+        type="password"
+        class="form-control"
+        id="password"
+        bind:value={formData.password}
+      />
       {#if errors.password}
         <p class="error">{errors.password}</p>
       {/if}
     </div>
 
-    <div class="form-group">
-      <label for="mobile">Mobile:</label>
-      <input type="text" id="mobile" class="form-control" bind:value={mobile} />
-      {#if errors.mobile}
-        <p class="error">{errors.mobile}</p>
-      {/if}
-    </div>
-
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
-
-  {#if submissionStatus === 'success'}
-    <div class="alert alert-success mt-4">
-      <h3>Form Submitted Successfully!</h3>
-      <p>First Name: {submissionData.firstName}</p>
-      <p>Surname: {submissionData.surname}</p>
-      <p>Email: {submissionData.email}</p>
-      <p>Password: {submissionData.password}</p>
-      <p>Mobile: {submissionData.mobile}</p>
-    </div>
-  {/if}
 </div>
+
